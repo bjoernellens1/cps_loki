d59e6f98119aee7e6d78ed0f32a13024e2c4e64f
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2024-03-25 10:07:43 +0100

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ sudo cp ps4_controller_script.sh /etc/udev/rules.d
 sudo chmod +x /etc/udev/rules.d/ps4_controller_script.sh
 
 # Create or append the udev rule
-echo 'ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="Wireless Controller", RUN+="/etc/udev/rules.d/ps4_controller_script.sh"' | sudo tee -a /etc/udev/rules.d/99-ps4-controller.rules
+echo 'ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="Wireless Controller", RUN+="/etc/udev/rules.d/ps4_controller_script.sh"' | sudo tee /etc/udev/rules.d/99-ps4-controller.rules
 
 # Reload udev rules
 sudo udevadm control --reload-rules
